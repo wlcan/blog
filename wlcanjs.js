6e81052f462976751f9447705b55e1b6331890db
wulincan <29978247@qq.com>
2013-05-11 19:13:13 +0800

    post3

---
+++ wlcanjs.js
@@ -0,0 +1,79 @@
+var timer=null;
+
+function moveTop(iTarget)
+{
+	var oNav=document.getElementById('nav');
+	
+	clearInterval(timer);
+	timer=setInterval(function(){
+			var speed=(iTarget-oNav.offsetTop)/6;
+			speed=speed>0?Math.ceil(speed):Math.floor(speed);
+			
+			if(oNav.offsetTop==iTarget)
+			{
+				clearInterval(timer);
+			}
+			else
+			{
+				oNav.style.top=oNav.offsetTop+speed+'px';
+			}
+		},30);
+}
+
+		
+function getStyle(obj,name)
+{
+	if(obj.currentStyle)
+	{
+		return obj.currentStyle[name];
+	}
+	else
+	{
+		return getComputedStyle(obj,false)[name];
+	}
+}
+
+function startMove(obj,json,fnEnd)
+{
+	clearInterval(obj.timer);
+	obj.timer=setInterval(function(){
+			var bStop=true;
+		
+			for(var attr in json)
+			{
+				var cur=0;
+				if(attr=='opacity')
+				{
+					cur=Math.round(parseFloat(getStyle(obj,attr))*100);
+				}
+				else
+				{
+					cur=parseInt(getStyle(obj,attr));
+				}
+				
+				var speed=(json[attr]-cur)/6;
+				speed=speed>0?Math.ceil(speed):Math.floor(speed);
+				
+				if(cur!=json[attr])
+					bStop=false;
+					
+				if(attr=='opacity')
+				{
+					obj.style.filter='alpha(opacity:'+(cur+speed)+')';
+					obj.style.opacity=(cur+speed)/100;
+				}
+				else
+				{
+					obj.style[attr]=cur+speed+'px';
+				}
+			}
+			
+			if(bStop)
+			{
+				clearInterval(obj.timer);
+				
+				if(fnEnd) 
+					fnEnd();
+			}
+		},30);
+}
